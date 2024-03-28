fd243a88f8c64d54012992d07ed1c61aba42828d
yobiiq-fostin <fostin.yobiiq@gmail.com>
2024-03-28 17:48:27 +0000

    Codec for new EM4301-CT device

--- Codec_EM4301-CT/Codec_EM4301-CT.js
+++ Codec_EM4301-CT/Codec_EM4301-CT.js
@@ -1,7 +1,7 @@
 /**
  * Codec for EM4301-CT device : compatible with TTN, ChirpStack v4 and v3, etc...
  * Release Date : 07 March 2024
- * Update  Date : 13 March 2024
+ * Update  Date : 27 March 2024
  */
 
 // Configuration constants for device basic info
@@ -420,7 +420,7 @@ function getValueFromBytesBigEndianFormat(bytes, index, size)
         value = (value | bytes[index+i]) << 8; 
     }
     value = value | bytes[index+size-1]
-    return value;
+    return (value >>> 0); // to unsigned
 }
 
 function getValueFromBytesLittleEndianFormat(bytes, index, size)
@@ -431,7 +431,7 @@ function getValueFromBytesLittleEndianFormat(bytes, index, size)
         value = (value | bytes[index+i]) << 8; 
     }
     value = value | bytes[index]
-    return value;
+    return (value >>> 0); // to unsigned
 }
 
 function getDigitStringArrayNoFormat(bytes, index, size)
